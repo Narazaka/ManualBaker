44b431edb49c39229c64b8ba173ca3d62642e6ea
Narazaka <info@narazaka.net>
2023-06-10 20:41:06 +0900

    impl

---
+++ Editor/ManualBaker.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using VRC.SDK3.Avatars.Components;
+using System.Reflection;
+
+namespace net.narazaka.vrchat.manualbaker
+{
+    public class ManualBaker
+    {
+        [MenuItem("Tools/Manual Bake!", true)]
+        static bool Check()
+        {
+            var avatar = Selection.activeGameObject;
+            return avatar && avatar.GetComponent<VRCAvatarDescriptor>() != null;
+        }
+
+        [MenuItem("Tools/Manual Bake!", false)]
+        static void Do()
+        {
+            EditorApplication.ExecuteMenuItem("Tools/Modular Avatar/Manual bake avatar");
+            EditorApplication.ExecuteMenuItem("Tools/Avatar Optimizer/Manual Bake Avatar");
+
+            var avatar = Selection.activeGameObject;
+            if (avatar == null || avatar.GetComponent<VRCAvatarDescriptor>() == null) return;
+            DestroyRecursive(avatar);
+        }
+
+        static void DestroyRecursive(GameObject subject)
+        {
+            var targets = FindDestroyTargets(subject);
+            foreach (var target in targets)
+            {
+                Object.DestroyImmediate(target, false);
+            }
+        }
+
+        static IEnumerable<GameObject> FindDestroyTargets(GameObject subject)
+        {
+            // FindGameObjectsWithTag does not return inactive GameObjects
+            if (subject.tag == "EditorOnly")
+            {
+                return new List<GameObject> { subject };
+            }
+            else
+            {
+                var targets = new List<GameObject>();
+                foreach (Transform child in subject.transform)
+                {
+                    targets.AddRange(FindDestroyTargets(child.gameObject));
+                }
+                return targets;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Manual Baker
+
+Modular AvatarとAvatar OptimizerのManual Bakeを実行して、ついでにEditorOnlyなやつを削除する。
+
+VRM作成時とかに便利。
+
+## インストール
+
+1. https://vpm.narazaka.net/ から「Add to VCC」ボタンを押してリポジトリをVCCにインストールします。
+2. VCCでSettings→Packages→Installed Repositoriesの一覧中で「Narazaka VPM Listing」にチェックが付いていることを確認します。
+3. アバタープロジェクトの「Manage Project」から「Manual Baker」をインストールします。
+
+## 使い方
+
+「Tools」→「Manual Bake!」をクリックします。
+
+## License
+
+[Zlib License](LICENSE.txt)
